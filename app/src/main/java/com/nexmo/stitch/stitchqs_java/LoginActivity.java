901061c477d72270c8d7db80363132e5fe5d0195
Britt Barak <britt.barak@gmail.com>
2018-07-23 13:35:13 +0100

    Initial commit

---
+++ app/src/main/java/com/nexmo/stitch/stitchqs_java/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.nexmo.stitch.stitchqs_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
